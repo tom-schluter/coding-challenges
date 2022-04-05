a37813f050a68a9bd752c6b6c6630d3ec7ec1aae
Tom Schuter <tom.schluter@outlook.com>
2022-04-05 10:41:47 +0200

    Coding challenges 05-04-2022 from codewars

---
+++ CC 2022-04-05/app.js
@@ -0,0 +1,55 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// Psuedo code
+
+// create counter
+
+// itterate over string letter by letter
+
+// compare current letter to vowel list
+
+// if it is a vowel add 1 to counter
+
+// return counter
+
+// first solution using regular function and for loop
+
+function getCount(str) {
+  let vowelsCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
+
+//   arrow function
+
+const getCount = (str) => {
+  let vowelsCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      vowelsCount++;
+    }
+  }
+  return vowelsCount;
+};
