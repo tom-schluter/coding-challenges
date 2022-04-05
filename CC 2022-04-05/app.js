0a8dce021f89a5df33b806cb7908a9f405757799
Tom Schuter <tom.schluter@outlook.com>
2022-04-05 12:44:29 +0200

    Coding challenges 05-04-2022 from codewars

--- CC 2022-04-05/app.js
+++ CC 2022-04-05/app.js
@@ -96,3 +96,41 @@ function disemvowel(str) {
 // then return the updated string
 
 const disemvowel = (str) => str.replace(/[aeiouAEIOU]/g, "");
+
+// ---------------------------------------
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+// psuedo code
+
+// create new total variable
+
+// iterate over input number by first turning it into a string and splitting it.
+// each individual digit needs to be turned back into a number and squared.
+// each squared number needs to be turned into a string
+
+// concatenate individual string number to total variable
+
+// return total variable
+
+function squareDigits(num) {
+  let total = ("" + num)
+    .split("")
+    .map((nr) => nr * nr)
+    .join("");
+  return Number(total);
+}
+
+//   arrow function version
+
+const squareDigits = (num) =>
+  Number(
+    ("" + num)
+      .split("")
+      .map((nr) => nr * nr)
+      .join("")
+  );
