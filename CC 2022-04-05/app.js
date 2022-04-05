5e4f7e8410f3550e6bfa9643c4627b28fc7c6782
Tom Schuter <tom.schluter@outlook.com>
2022-04-05 11:53:03 +0200

    Coding challenges 05-04-2022 from codewars

--- CC 2022-04-05/app.js
+++ CC 2022-04-05/app.js
@@ -53,3 +53,46 @@ const getCount = (str) => {
   }
   return vowelsCount;
 };
+
+// -----------------------
+
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+
+// psuedo code
+
+// method 1:
+// create new empty string
+// iterate over the individual characters of the given string
+// check if current character is a vowel, continue loop, if not add it to the new string
+// return new string
+function disemvowel(str) {
+  let noVowelString = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i].toLowerCase() === "a" ||
+      str[i].toLowerCase() === "e" ||
+      str[i].toLowerCase() === "i" ||
+      str[i].toLowerCase() === "o" ||
+      str[i].toLowerCase() === "u"
+    ) {
+      continue;
+    } else {
+      noVowelString += str[i];
+    }
+  }
+  return noVowelString;
+}
+
+// method 2:
+// take string and replace certain characters with an empty character
+// then return the updated string
+
+const disemvowel = (str) => str.replace(/[aeiouAEIOU]/g, "");
