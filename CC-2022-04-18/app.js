98b90c1b93903b5fe872c540414fee7605263889
Tom Schuter <tom.schluter@outlook.com>
2022-04-18 14:51:41 +0200

    Coding challenges 18-04-2022 from codewars

--- CC-2022-04-18/app.js
+++ CC-2022-04-18/app.js
@@ -83,3 +83,59 @@ const printerError = (s) => {
 
 const openOrSenior = (data) =>
   data.map((person) => (person[0] >= 55 && person[1] > 7 ? "Senior" : "Open"));
+
+// ----------------------------------------------------------
+
+//   Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// simply compare the ending of the first string to the second string
+
+// return true or false accordinly
+
+function solution(str, ending) {
+  if (str.endsWith(ending)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const solution = (str, ending) => (str.endsWith(ending) ? true : false);
+
+// -------------------------------------------------------------
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// psuedo code
+
+// convert array to string
+
+// convert binary string to number
+
+// return number
+
+const binaryArrayToNumber = (arr) => {
+  let num = arr.join("");
+  return parseInt(num, 2);
+};
+
+const binaryArrayToNumber = (arr) => parseInt(arr.join(""), 2);
