ae352d998d7cbc415ad01611a3d8b3afeb7aeb5c
Tom Schuter <tom.schluter@outlook.com>
2022-04-19 23:52:41 +0200

    Coding challenges 19-04-2022 from codewars

--- CC-2022-04-19/app.js
+++ CC-2022-04-19/app.js
@@ -27,3 +27,37 @@ const oddOrEven = (array) =>
   array.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd";
 
 // -------------------------------------------------------------------
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output= [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+// psuedo code
+
+// create a new array
+
+// iterate over the old array but remove the lowest value and push the  rest to new array
+
+// return new array
+
+function removeSmallest(numbers) {
+  let newArr = [...numbers];
+  if (numbers === []) {
+    newArr = [];
+  } else {
+    let min = Math.min(...numbers);
+    newArr.splice(newArr.indexOf(min), 1);
+  }
+  return newArr;
+}
