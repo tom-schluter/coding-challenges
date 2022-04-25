831f10d718507387a77e2d576e7b97d455dac912
Tom Schuter <tom.schluter@outlook.com>
2022-04-25 10:55:21 +0200

    Coding challenges 25-04-2022 from codewars

--- CC-2022-04-25/app.js
+++ CC-2022-04-25/app.js
@@ -1,18 +1,59 @@
-// Your task is to make function, which returns the sum of a sequence of integers.
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
 
-// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+// For example: (Input --> Output)
 
-// If begin value is greater than the end, function should returns 0
-
-// Examples
-
-// 2,2,2 --> 2
-// 2,6,2 --> 12 (2 + 4 + 6)
-// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
-// 1,5,3  --> 5 (1 + 4)
-// This is the first kata in the series:
-
-// Sum of a sequence (this kata)
-// Sum of a Sequence [Hard-Core Version]
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
 
 // psuedo code
+
+// turn the number into a string and split it
+
+// turn them back into numbers and add them up
+
+// return the total
+
+function sumDigits(number) {
+  if (number < 0) {
+    number = number * -1;
+  }
+  number = number.toString();
+  let numArr = number.split("");
+  let result = numArr.reduce((prev, cur) => Number(prev) + Number(cur), 0);
+  return result;
+}
+
+// ----------------------------------------------------------------------
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+// psuedo code
+
+// first identify if there is # present
+
+// if there is find the index of it
+
+// remove from that index to the end of the string
+
+// return string
+
+function removeUrlAnchor(url) {
+  if (url.includes("#")) {
+    let index = url.indexOf("#");
+    const newStr = url.substring(0, index);
+    return newStr;
+  } else {
+    return url;
+  }
+}
+
+// or
+
+const removeUrlAnchor = (url) =>
+  url.includes("#") ? url.substring(0, url.indexOf("#")) : url;
