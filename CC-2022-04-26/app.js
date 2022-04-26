bb2829b3b353be977e0c72e13ddf92aabef937e2
Tom Schuter <tom.schluter@outlook.com>
2022-04-26 10:36:58 +0200

    Coding challenges 26-04-2022 from codewars

--- CC-2022-04-26/app.js
+++ CC-2022-04-26/app.js
@@ -39,3 +39,33 @@ var capitals = function (word) {
 // use mod % to determine even numbers
 
 // return new array
+
+function noOdds(values) {
+  let evenArr = [];
+  values.forEach((num) => {
+    if (num % 2 === 0) {
+      evenArr.push(num);
+    }
+  });
+  return evenArr;
+}
+
+// -----------------------------------------------------------
+
+// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+// For example, if this array were passed as an argument:
+
+// ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+// Your function would return the following array:
+
+// ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+// All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+// psuedo code
+
+// literally sort by length
+
+const sortByLength = (array) => array.sort((a, b) => a.length - b.length);
