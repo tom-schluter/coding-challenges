8dfcca50b12d6828460dfe4f1568fc2ecf718c2c
Tom Schuter <tom.schluter@outlook.com>
2022-05-10 00:12:54 +0200

    Coding challenges 09-05-2022 from codewars

--- CC-2022-05-09/app.js
+++ CC-2022-05-09/app.js
@@ -12,3 +12,23 @@
 // compare the initital array t oa sorted descending array
 
 // elsewise return no
+
+function isSortedAndHow(array) {
+  let ascending = [...array].sort((a, b) => a - b);
+  let descending = [...array].sort((a, b) => b - a);
+  if (array.toString() == ascending.toString()) {
+    return "yes, ascending";
+  } else if (array.toString() == descending.toString()) {
+    return "yes, descending";
+  } else {
+    return "no";
+  }
+}
+
+// -----------------------------------------------------
+
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+sortme = function (names) {
+  return names.sort();
+};
