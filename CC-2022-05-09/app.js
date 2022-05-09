28d8ed8920f61bb1fc3bc43f62b1c4a15c3ea4bc
Tom Schuter <tom.schluter@outlook.com>
2022-05-09 23:44:13 +0200

    Coding challenges 09-05-2022 from codewars

---
+++ CC-2022-05-09/app.js
@@ -0,0 +1,14 @@
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+// psuedo code
+
+// compare the initial array to a sorted ascending array
+
+// compare the initital array t oa sorted descending array
+
+// elsewise return no
