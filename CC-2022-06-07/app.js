fae4c53609a0e98dc58298b4b2ff68afcaf018a1
Tom Schuter <tom.schluter@outlook.com>
2022-06-07 15:44:05 +0200

    Coding challenges 07-06-2022 from codewars

---
+++ CC-2022-06-07/app.js
@@ -0,0 +1,44 @@
+// Write a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
+
+// calculate(2,"+", 4); //Should return 6
+// calculate(6,"-", 1.5); //Should return 4.5
+// calculate(-4,"*", 8); //Should return -32
+// calculate(49,"/", -7); //Should return -7
+// calculate(8,"m", 2); //Should return null
+// calculate(4,"/",0) //should return null
+// Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null (throw an ArgumentException in C#)/(None in Python).
+
+// parameters
+
+// an integer, an operator (possibly something else) and another integer
+
+// returns
+
+// the completed sum, unless the wrong operator is given or there is division by 0, in which case return null
+
+// examples, see above
+
+// psuedo code
+
+// write switch case handling all operators and if the wrong operator is given default to returning null
+
+// in the division case if the second number is 0 return null
+
+function calculate(num1, operation, num2) {
+  switch (operation) {
+    case "+":
+      return num1 + num2;
+      break;
+    case "-":
+      return num1 - num2;
+      break;
+    case "*":
+      return num1 * num2;
+      break;
+    case "/":
+      return num2 === 0 ? null : num1 / num2;
+      break;
+    default:
+      return null;
+  }
+}
