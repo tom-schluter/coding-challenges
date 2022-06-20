36066b0027ae6cb209640f7748fed85ce8a907a7
Tom Schuter <tom.schluter@outlook.com>
2022-06-20 11:03:52 +0200

    coding challenges 20-06-2022 from codewars

---
+++ CC-2022-06-20/app.js
@@ -0,0 +1,30 @@
+// ASC Week 1 Challenge 4 (Medium #1)
+
+// Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+// Note that spaces should be ignored in this case.
+
+// Examples
+// "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+// // "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
+
+// parameters
+
+// a string (sentence)
+
+// returns
+
+// the same string but with spaces removed and 2 spaces added between all letters, the letters converted to uppercase
+
+// examples, see above
+
+// psuedo code
+
+// remove all spaces
+
+// convert to uppercase
+
+// add 2 spaces between each character
+
+const vaporcode = (string) =>
+  string.replace(/ /g, "").toUpperCase().split("").join("  ");
